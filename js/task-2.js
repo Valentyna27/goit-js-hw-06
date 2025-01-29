c552877016c58561ae380074db68a1a8d4a28b94
Valentyna <valia.melnik@gmail.com>
2025-01-29 20:03:06 +0100

    new

--- js/task-2.js
+++ js/task-2.js
@@ -9,18 +9,16 @@ class Storage {
   }
 
   addItem(newItem) {
-    this.#items.push(newItem);
+    return this.#items.push(newItem);
   }
 
   removeItem(itemToRemove) {
-    /* Перший варіант
-   this.#items = this.#items.filter((item) => item !== itemToRemove); */
+     this.#items = this.#items.filter((item) => item !== itemToRemove);
 
-    /* Другий варіант */
-    const removeItemIndex = this.#items.indexOf(itemToRemove);
-    if (removeItemIndex !== -1) {
-      this.#items.splice(removeItemIndex, 1);
-    }
+    // const removeItemIndex = this.#items.indexOf(itemToRemove);
+    // if (removeItemIndex !== -1) {
+    //   this.#items.splice(removeItemIndex, 1);
+    // }
   }
 }
 
